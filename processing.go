7518c57d7dc3542ecbe0e65f43195e7f854bfd0e
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-08-19 14:53:38 +0300

    update processing interface and add mockery

--- processing.go
+++ processing.go
@@ -2,8 +2,12 @@ package worker
 
 import "context"
 
+// Processing defines an interface for processing tasks.
+// It includes methods for performing the main processing logic and handling errors.
+//
+//go:generate mockery --name=Processing
 type Processing interface {
-	Processing(ctx context.Context, payload interface{})
+	Processing(ctx context.Context, input interface{}) (interface{}, error)
 
-	ErrorHandler(ctx context.Context, payload interface{})
+	ErrorHandler(ctx context.Context, input interface{}) (interface{}, error)
 }
