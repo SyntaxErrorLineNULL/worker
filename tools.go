31e6d1aac46cc6eec37003640ec0bd44594e1989
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-09-03 23:24:26 +0300

    fix linter error

--- tools.go
+++ tools.go
@@ -3,11 +3,11 @@ package worker
 // GetRecoverError extracts an error from a recoverable panic.
 // It checks if the recovered value is an error type, and if so, returns it.
 // If the recovered value is not an error type, it returns nil.
-func GetRecoverError(recover any) error {
+func GetRecoverError(rec any) error {
 	// Check if recoverable value is not nil
-	if recover != nil {
+	if rec != nil {
 		// Type switch on the recovered value
-		switch e := recover.(type) {
+		switch e := rec.(type) {
 		// If recovered value is of type error
 		case error:
 			return e
