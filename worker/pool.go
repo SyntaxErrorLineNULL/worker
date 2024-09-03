8ca7c3efd98c31b74311b3268949a2de950445ef
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-09-03 23:57:10 +0300

    retry restart worker

--- worker/pool.go
+++ worker/pool.go
@@ -12,20 +12,21 @@ import (
 )
 
 type Pool struct {
-	ctx               context.Context    // Parent context for the pool.
-	contextCancelFunc context.CancelFunc // Cancel function for the pool's context.
-	taskQueue         chan worker.Task   // Channel for queueing task's.
-	workers           []worker.Worker    // Slice to hold worker instances.
-	maxWorkersCount   int32              // Maximum number of workers in the pool.
-	workerConcurrency atomic.Int32       // Number of currently running workers.
-	workerWg          *sync.WaitGroup
-	stopCh            chan struct{}      // Channel to signal stopping the pool.
-	mutex             sync.Mutex         // Mutex for locking access to the pool.
-	onceStart         sync.Once          // Used for a one-time action (starting the pool).
-	onceStop          sync.Once          // Used for a one-time action (stopping the pool).
-	stopped           bool               // flag signaling that the worker pool has already been stopped. The flag is needed to understand whether it is necessary to restore some worker after its fall, perhaps the worker pool is already stopped.
-	workerErrorCh     chan *worker.Error // A channel for worker error. It is needed so that in case of panic we can restore the Worker's operation.
-	logger            *log.Logger
+	ctx                   context.Context    // Parent context for the pool.
+	contextCancelFunc     context.CancelFunc // Cancel function for the pool's context.
+	taskQueue             chan worker.Task   // Channel for queueing task's.
+	workers               []worker.Worker    // Slice to hold worker instances.
+	maxWorkersCount       int32              // Maximum number of workers in the pool.
+	workerConcurrency     atomic.Int32       // Number of currently running workers.
+	maxRetryWorkerRestart int
+	workerWg              *sync.WaitGroup
+	stopCh                chan struct{}      // Channel to signal stopping the pool.
+	mutex                 sync.Mutex         // Mutex for locking access to the pool.
+	onceStart             sync.Once          // Used for a one-time action (starting the pool).
+	onceStop              sync.Once          // Used for a one-time action (stopping the pool).
+	stopped               bool               // flag signaling that the worker pool has already been stopped. The flag is needed to understand whether it is necessary to restore some worker after its fall, perhaps the worker pool is already stopped.
+	workerErrorCh         chan *worker.Error // A channel for worker error. It is needed so that in case of panic we can restore the Worker's operation.
+	logger                *log.Logger
 }
 
 func NewWorkerPool(options *worker.Options) *Pool {
