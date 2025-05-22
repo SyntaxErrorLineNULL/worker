0874bc2e4830541250cd7be4a772b763647ddd6e
syntaxErrorLineNULL <cyberorange16@gmail.com>
2025-05-22 14:18:21 +0300

    worker options pkg

---
+++ options.go
@@ -0,0 +1,38 @@
+package worker
+
+import (
+	"context"
+	"log"
+)
+
+// DefaultMaxRetry is the default number of times a worker is allowed to restart
+// after encountering a failure. If not explicitly set in the worker pool options,
+// this value is used to control the maximum retry attempts for restarting workers.
+const DefaultMaxRetry = 3
+
+// Options defines the configuration settings for a worker pool. It contains
+// the context for task processing, the task queue, and settings related to
+// the number of workers and retry behavior.
+type Options struct {
+	// Context is the context used to control the lifecycle of the tasks and workers.
+	// It allows for task cancellation, timeouts, and passing values between functions.
+	Context context.Context
+
+	// Queue is the channel where tasks are submitted to be processed by the workers.
+	// The size of the channel buffer determines how many tasks can be queued before
+	// workers are required to start processing them.
+	//
+	// Note: the channel must be closed after the worker pool is stopped.
+	Queue chan Task
+	// WorkerCount specifies the number of workers that will be spawned in the pool.
+	// This determines how many tasks can be processed concurrently.
+	WorkerCount int32
+	// MaxRetryWorkerRestart defines the maximum number of times a worker can be restarted
+	// in case of failure before it is considered as a critical issue and further restarts
+	// are stopped. This helps in preventing endless restarts in case of persistent errors.
+	MaxRetryWorkerRestart int32
+	// Logger provides a logging mechanism for the worker pool. It is used to log
+	// significant events, errors, and debugging information, aiding in monitoring
+	// and troubleshooting the system.
+	Logger *log.Logger
+}
